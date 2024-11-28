d96ad7cde6a415e2fe4e7439cf200c0e6536f5f2
pedro amaral <amaralph1995@gmail.com>
2024-11-28 12:31:08 -0300

    create orderService

--- src/main/java/com/devsuperior/ds_order_system/services/OrderService.java
+++ src/main/java/com/devsuperior/ds_order_system/services/OrderService.java
@@ -1,8 +1,14 @@
 package com.devsuperior.ds_order_system.services;
 
+import com.devsuperior.ds_order_system.entities.Order;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+
+    public Order total(Order order) {
+        order.setBasic(order.getBasic() * (1 - (order.getDiscount()/100)));
+        return order;
+    }
 
 }
