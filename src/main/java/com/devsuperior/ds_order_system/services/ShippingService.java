06dab52b0522ea83e742466ef95b13d0b73e5d2a
pedro amaral <amaralph1995@gmail.com>
2024-11-28 12:31:28 -0300

    create ShippingService

--- src/main/java/com/devsuperior/ds_order_system/services/ShippingService.java
+++ src/main/java/com/devsuperior/ds_order_system/services/ShippingService.java
@@ -1,7 +1,24 @@
 package com.devsuperior.ds_order_system.services;
 
+import com.devsuperior.ds_order_system.entities.Order;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ShippingService {
+    private double FREE_SHIPPING_ORDER_VALUE = 200.0;
+    private double MIN_VALUE_FOR_SHIPPING_DISCOUNT = 100.0;
+    public Order shipment(Order order) {
+        double orderPrice = order.getBasic();
+
+        if (orderPrice > FREE_SHIPPING_ORDER_VALUE) { return order; }
+
+        if (orderPrice < MIN_VALUE_FOR_SHIPPING_DISCOUNT) {
+            order.setBasic(orderPrice + 20.0);
+        } else {
+            order.setBasic(orderPrice + 12.0);
+        }
+
+        return order;
+    }
+
 }
